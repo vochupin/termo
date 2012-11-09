43f895b81981d41236e76fdf6408b9745bf13568
vc <vc@192.168.0.196>
2012-11-09 18:04:45 +0700

    вывод с консоли в текстовое поле

--- crashreport/src/com/example/crashreport/RssParser.java
+++ crashreport/src/com/example/crashreport/RssParser.java
@@ -14,100 +14,55 @@ import com.google.code.rome.android.repackaged.com.sun.syndication.io.XmlReader;
 
 public class RssParser {
 	public SyndFeed parseFeed(String url) throws IllegalArgumentException, MalformedURLException, FeedException, IOException {
-	        return new SyndFeedInput().build(new XmlReader(new URL(url)));
+		return new SyndFeedInput().build(new XmlReader(new URL(url)));
+	}
 
-	    }
+	public String toString(SyndFeed feed){
+		
+		StringBuffer buf = new StringBuffer();
 
+		buf.append("About feed:\n");
 
+		buf.append("Author: " + feed.getAuthor() + "\n");
 
-	    public void printRSSContent(SyndFeed feed)
+		buf.append("Authors:\n");
 
-	    {
+		if (feed.getAuthors() != null){
 
-	        System.out.println("About feed:");
+			for (Object author : feed.getAuthors())	{
 
-	        System.out.println("Author: " + feed.getAuthor());
+				buf.append(((SyndPerson) author).getName() + "\n");
+				buf.append(((SyndPerson) author).getEmail() + "\n");
+				buf.append(((SyndPerson) author).getUri() + "\n");
+				buf.append("\n");
+			}
+		}
 
-	        System.out.println("Authors:");
+		buf.append("Title: " + feed.getTitle() + "\n");
+		buf.append("Title Ex: " + feed.getTitleEx() + "\n");
+		buf.append("Description: " + feed.getDescription() + "\n");
+		buf.append("Description Ex: " + feed.getDescriptionEx() + "\n");
+		buf.append("Date" + feed.getPublishedDate() + "\n");
+		buf.append("Type: " + feed.getFeedType() + "\n");
+		buf.append("Encoding: " + feed.getEncoding() + "\n");
+		buf.append("(C) " + feed.getCopyright() + "\n");
+		buf.append("\n");
+		for (Object object : feed.getEntries()){
+			SyndEntry entry = (SyndEntry) object;
+			buf.append(entry.getTitle() + " - " + entry.getAuthor() + "\n");
+			buf.append(entry.getLink() + "\n");
+			for (Object contobj : entry.getContents()){
+				SyndContent content = (SyndContent) contobj;
+				buf.append(content.getType() + "\n");
+				buf.append(content.getValue() + "\n");
+			}
 
-	        if (feed.getAuthors() != null)
-
-	        {
-
-	            for (Object author : feed.getAuthors())
-
-	            {
-
-	                System.out.println(((SyndPerson) author).getName());
-
-	                System.out.println(((SyndPerson) author).getEmail());
-
-	                System.out.println(((SyndPerson) author).getUri());
-
-	                System.out.println();
-
-	            }
-
-	        }
-
-	        System.out.println("Title: " + feed.getTitle());
-
-	        System.out.println("Title Ex: " + feed.getTitleEx());
-
-	        System.out.println("Description: " + feed.getDescription());
-
-	        System.out.println("Description Ex: " + feed.getDescriptionEx());
-
-	        System.out.println("Date" + feed.getPublishedDate());
-
-	        System.out.println("Type: " + feed.getFeedType());
-
-	        System.out.println("Encoding: " + feed.getEncoding());
-
-	        System.out.println("(C) " + feed.getCopyright());       
-
-	        System.out.println();
-
-
-
-	        for (Object object : feed.getEntries())
-
-	        {
-
-	            SyndEntry entry = (SyndEntry) object;
-
-	            System.out.println(entry.getTitle() + " - " + entry.getAuthor());
-
-	            System.out.println(entry.getLink());
-
-	            for (Object contobj : entry.getContents())
-
-	            {
-
-	                SyndContent content = (SyndContent) contobj;
-
-	                System.out.println(content.getType());
-
-	                System.out.println(content.getValue());
-
-	            }
-
-
-
-	            SyndContent content = entry.getDescription();
-
-	            if (content != null)
-
-	                System.out.println(content.getValue());
-
-
-
-	            System.out.println(entry.getPublishedDate());
-
-	            System.out.println();
-
-	        }
-
-	    }
-
+			SyndContent content = entry.getDescription();
+			if (content != null) buf.append(content.getValue());
+			buf.append(entry.getPublishedDate() + "\n");
+			buf.append("\n");
+		}
+		
+		return buf.toString();
+	}
 }
