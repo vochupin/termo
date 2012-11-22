4bd5de1065273ed2c0cfe985ee318cdddd4f6aa4
vc <vc@desktop>
2012-11-23 01:07:49 +0700

    [FIX] исправление ошибки запуска сетевой операции из UI потока

--- termo/src/com/vochupin/termo/UpdateService.java
+++ termo/src/com/vochupin/termo/UpdateService.java
@@ -12,7 +12,13 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import 	android.os.Process;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.RemoteViews;
@@ -23,39 +29,78 @@ public class UpdateService extends Service {
 	private static final String TAG = UpdateService.class.getSimpleName();
 	private static final String TERMO_SERVER = "http://termo.tomsk.ru/";
 	private static final String TERMO_JSON_INFORMER = "data.json";
+	private static final String ALL_WIDGET_IDS = "allWidgetIds";
+
+	private Looper mServiceLooper;
+	private ServiceHandler mServiceHandler;
+
+	// Handler that receives messages from the thread
+	private final class ServiceHandler extends Handler {
+		public ServiceHandler(Looper looper) {
+			super(looper);
+		}
+		@Override
+		public void handleMessage(Message msg) {
+			Log.i(TAG, "Update service started.");
+			
+			AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(UpdateService.this.getApplicationContext());
+			int[] allWidgetIds = msg.getData().getIntArray(ALL_WIDGET_IDS);
+			Log.w(TAG, "Number of ids to update (from intent): " + String.valueOf(allWidgetIds.length));
+	
+			//Request JSON with temperature
+			Client client = new Client(TERMO_SERVER);
+			String tempJson = client.getTemperatureJson(TERMO_JSON_INFORMER);
+			
+			String temperature = parseTermoResponse(tempJson);
+			
+			updateWidgets(appWidgetManager, allWidgetIds, temperature);
+
+			stopSelf(msg.arg1);
+			Log.i(TAG, "handler done"); 
+		}
+	}
 
 	@Override
-	public void onStart(Intent intent, int startId) {
-		Log.i(TAG, "Update service started.");
+	public void onCreate() {
+		HandlerThread thread = new HandlerThread("ServiceStartArguments", Process.THREAD_PRIORITY_BACKGROUND);
+		thread.start();
 
-		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this.getApplicationContext());
+		mServiceLooper = thread.getLooper();
+		mServiceHandler = new ServiceHandler(mServiceLooper);
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		Message msg = mServiceHandler.obtainMessage();
+		msg.arg1 = startId;
+		
 		int[] allWidgetIds = intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
-		Log.w(TAG, "Number of ids to update (from intent): " + String.valueOf(allWidgetIds.length));
+		Bundle bundle = new Bundle();
+		bundle.putIntArray(ALL_WIDGET_IDS, allWidgetIds);
+		msg.setData(bundle);
+		
+		mServiceHandler.sendMessage(msg);
 
-		//Request JSON with temperature
-		Client client = new Client(TERMO_SERVER);
-		String tempJson = client.getTemperatureJson(TERMO_JSON_INFORMER);
-		
-		String temperature = parseTermoResponse(tempJson);
-		
-		updateWidgets(appWidgetManager, allWidgetIds, temperature);
-		
-		stopSelf();
+		return START_STICKY;
+	}
 
-		super.onStart(intent, startId);
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+
+	@Override
+	public void onDestroy() {
+		mServiceHandler.removeCallbacksAndMessages(null);
+		mServiceLooper.quit();
+		
+		Log.i(TAG, "service done"); 
 	}
 
 	private void updateWidgets(AppWidgetManager appWidgetManager, int[] allWidgetIds, String temperature) {
 		for (int widgetId : allWidgetIds) {
 			RemoteViews remoteViews = new RemoteViews(this.getApplicationContext().getPackageName(), R.layout.main);
 			remoteViews.setTextViewText(R.id.tvOutput, temperature);
-			
-//			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-//		    String value = prefs.getString("", null);
-//		    if(value != null)
-//		    {
-//		        // do stuff
-//		    }
 
 			enableUpdateOnClick(allWidgetIds, remoteViews);
 
@@ -77,20 +122,15 @@ public class UpdateService extends Service {
 		String retval;
 		try {
 			JSONObject jobj = new JSONObject(temperature);
-			
+
 			retval = "t° = " + jobj.getDouble("current_temp");
 			retval += "     Δt = " + jobj.getString("current_temp_change");
 			retval += "\n" + jobj.getString("current_date") + " " + jobj.getString("current_time");
-			
+
 		} catch (JSONException e) {
 			e.printStackTrace();
 			retval = "JSON error.";
 		}
 		return retval;
-	}
-
-	@Override
-	public IBinder onBind(Intent intent) {
-		return null;
 	}
 } 
