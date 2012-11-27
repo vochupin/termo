580eed086782e25f626b0a9d842007bd9f7d2016
vc <vc@192.168.0.196>
2012-11-27 11:42:39 +0700

    ignores

--- termo/src/com/vochupin/termo/UpdateService.java
+++ termo/src/com/vochupin/termo/UpdateService.java
@@ -1,7 +1,16 @@
 package com.vochupin.termo;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import com.vochupin.termo.db.TermoDatasource;
+import com.vochupin.termo.db.TermoSample;
 
 import android.app.PendingIntent;
 import android.app.Service;
@@ -27,6 +36,10 @@ public class UpdateService extends Service {
 	private Looper mServiceLooper;
 	private ServiceHandler mServiceHandler;
 
+	private Map<String, Integer> trendMap = new HashMap<String, Integer>(){{
+		put("+", +1); put("-", -1);
+	}};
+
 	// Handler that receives messages from the thread
 	private final class ServiceHandler extends Handler {
 		public ServiceHandler(Looper looper) {
@@ -35,18 +48,28 @@ public class UpdateService extends Service {
 		@Override
 		public void handleMessage(Message msg) {
 			Log.i(TAG, "Update service started.");
-			
+
 			AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(UpdateService.this.getApplicationContext());
 			int[] allWidgetIds = msg.getData().getIntArray(ALL_WIDGET_IDS);
 			Log.w(TAG, "Number of ids to update (from intent): " + String.valueOf(allWidgetIds.length));
-	
+
 			//Request JSON with temperature
 			Client client = new Client(TERMO_SERVER);
 			String tempJson = client.getTemperatureJson(TERMO_JSON_INFORMER);
-			
+
 			if(Client.NO_CONNECTION.equals(tempJson) == false){
-				String temperature = parseTermoResponse(tempJson);
-				updateWidgets(appWidgetManager, allWidgetIds, temperature);
+
+				TermoDatasource tds = new TermoDatasource(UpdateService.this);
+				tds.open();
+
+				TermoSample ts;
+				try {
+					ts = parseTermoResponse(tempJson, tds);
+					updateWidgets(appWidgetManager, allWidgetIds, ts.toString());
+				} catch (Exception e) {
+					Log.e(TAG, "Parsing error: " + e.toString() + " when parse: " + tempJson);
+				}
+				tds.close();
 			}
 
 			stopSelf(msg.arg1);
@@ -67,12 +90,12 @@ public class UpdateService extends Service {
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		Message msg = mServiceHandler.obtainMessage();
 		msg.arg1 = startId;
-		
+
 		int[] allWidgetIds = intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
 		Bundle bundle = new Bundle();
 		bundle.putIntArray(ALL_WIDGET_IDS, allWidgetIds);
 		msg.setData(bundle);
-		
+
 		mServiceHandler.sendMessage(msg);
 
 		return START_STICKY;
@@ -87,7 +110,7 @@ public class UpdateService extends Service {
 	public void onDestroy() {
 		mServiceHandler.removeCallbacksAndMessages(null);
 		mServiceLooper.quit();
-		
+
 		Log.i(TAG, "service done"); 
 	}
 
@@ -112,19 +135,22 @@ public class UpdateService extends Service {
 		remoteViews.setOnClickPendingIntent(R.id.tvOutput, pendingIntent);
 	}
 
-	private String parseTermoResponse(String temperature) {
-		String retval;
-		try {
-			JSONObject jobj = new JSONObject(temperature);
+	private TermoSample parseTermoResponse(String json, TermoDatasource tds) throws ParseException, JSONException {
+		TermoSample retval = null;
 
-			retval = "t° = " + jobj.getDouble("current_temp");
-			retval += "     Δt = " + jobj.getString("current_temp_change");
-			retval += "\n" + jobj.getString("current_date") + " " + jobj.getString("current_time");
+		JSONObject jobj = new JSONObject(json);
 
-		} catch (JSONException e) {
-			e.printStackTrace();
-			retval = "JSON error.";
-		}
+		float temperature = (float)jobj.getDouble("current_temp");
+
+		String trendString = jobj.getString("current_temp_change").trim();
+		int trend = trendMap.get(trendString);
+
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:dd.MM.yyyy");
+		String sampleTimeString = jobj.getString("current_time") + ":" + jobj.getString("current_date");
+
+		Date sampleTime = sdf.parse(sampleTimeString);
+
+		retval = tds.createSample(temperature, trend, sampleTime);
 		return retval;
 	}
 } 
