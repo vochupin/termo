9ee9b0f6baa62a1e5045d6d23d5e238c15c72bcd
vc <vc@chupin.uvaresearch.com>
2013-01-31 13:25:59 +0700

    [DEV] кнопка "готово"

--- termo/src/com/vochupin/termo/TermoWidget.java
+++ termo/src/com/vochupin/termo/TermoWidget.java
@@ -8,21 +8,21 @@ import android.content.Intent;
 import android.util.Log;
 
 public class TermoWidget extends AppWidgetProvider {
-    private static final String TAG = TermoWidget.class.getSimpleName();
+	private static final String TAG = TermoWidget.class.getSimpleName();
 
 	@Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 
-    	 Log.i(TAG, "onUpdate method called");
-    	    // Get all ids
-    	    ComponentName thisWidget = new ComponentName(context, TermoWidget.class);
-    	    int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+		Log.i(TAG, "onUpdate method called");
+		// Get all ids
+		ComponentName thisWidget = new ComponentName(context, TermoWidget.class);
+		int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
 
-    	    // Build the intent to call the service
-    	    Intent intent = new Intent(context.getApplicationContext(), UpdateService.class);
-    	    intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
+		// Build the intent to call the service
+		Intent intent = new Intent(context.getApplicationContext(), UpdateService.class);
+		intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
 
-    	    // Update the widgets via the service
-    	    context.startService(intent);
-    }
+		// Update the widgets via the service
+		context.startService(intent);
+	}
 }
